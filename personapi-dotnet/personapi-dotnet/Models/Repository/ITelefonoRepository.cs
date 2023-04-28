c75140cbdc0cd2f4e766113bf032cc57d937da85
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-04-28 00:40:30 -0500

    Implementado todo pero falta que funcione Estudios y Telefonos

---
+++ personapi-dotnet/personapi-dotnet/Models/Repository/ITelefonoRepository.cs
@@ -0,0 +1,13 @@
+﻿using personapi_dotnet.Models.Entities;
+
+namespace personapi_dotnet.Models.Repository
+{
+    public interface ITelefonoRepository
+    {
+        Task<Persona> CreateTelefono(Telefono telefono, Persona persona);
+        Task<bool> DeleteTelefono(Telefono telefono);
+        Persona GetTelefono(int id);
+        Task<bool> UpdatePersona(Telefono telefono);
+        IEnumerable<Telefono> GetTelefono();
+    }
+}
---
+++ personapi-dotnet/personapi-dotnet/Models/Repository/TelefonoRepository.cs
@@ -0,0 +1,37 @@
+﻿using personapi_dotnet.Models.Entities;
+
+namespace personapi_dotnet.Models.Repository
+{
+    public class TelefonoRepository : ITelefonoRepository
+    {
+        public Task<Persona> CreateTelefono(Telefono telefono)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Persona> CreateTelefono(Telefono telefono, Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTelefono(Telefono telefono)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Persona GetTelefono(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Telefono> GetTelefono()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdatePersona(Telefono telefono)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
