cd483840643455cb2954b224fc4ac24397ce4532
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-04-27 23:51:05 -0500

    Crud de Persona OK

--- personapi-dotnet/personapi-dotnet/Models/Repository/IProfesionRepository.cs
+++ personapi-dotnet/personapi-dotnet/Models/Repository/IProfesionRepository.cs
@@ -11,5 +11,3 @@ namespace personapi_dotnet.Models.Repository
 
     }
 }
-
-
---
+++ personapi-dotnet/personapi-dotnet/Models/Repository/PersonaRepository.cs
@@ -0,0 +1,32 @@
+﻿using personapi_dotnet.Models.Entities;
+
+namespace personapi_dotnet.Models.Repository
+{
+    public class PersonaRepository : IPersonaRepository
+    {
+        public Task<Persona> CreatePersona(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletePersona(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Persona GetPersona(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Persona> GetPersona()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdatePersona(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
